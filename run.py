056ffc5a2be056edd202ad7712339fa2178e55f6
Jefferson <jeeeff07@gmail.com>
2025-11-03 22:05:51 -0300

    Preparar para deploy no Render

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    print('🚀 API de Tarefas rodando em http://127.0.0.1:5000')
-    print('📚 Documentação: http://127.0.0.1:5000/')
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    print(f'🚀 API de Tarefas rodando em http://0.0.0.0:{port}')
+    print('📚 Documentação: http://0.0.0.0:{port}/')
+    app.run(debug=False, host='0.0.0.0', port=port)
